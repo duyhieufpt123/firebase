de4a4c0f5ebf0a2a1003733139e85271cd9fc3fd
Duy Hieu <buihieuduya4@gmail.com>
2023-03-20 11:03:39 +0700

    cont

--- src/firebase.js
+++ src/firebase.js
@@ -2,14 +2,14 @@ import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getAuth } from "firebase/auth";
 const firebaseConfig = {
-    apiKey: "AIzaSyC6Gq2EzKbeI4DQFau6dlJkUrgFh68ew1M",
-    authDomain: "lab7-367904.firebaseapp.com",
-    projectId: "lab7-367904",
-    storageBucket: "lab7-367904.appspot.com",
-    messagingSenderId: "730437167398",
-    appId: "1:730437167398:web:dfdd83588c45295fbaf9e7",
-    measurementId: "G-LHG411F6FL"
-  };
+  apiKey: "AIzaSyCCVgrMO089JFdaOCypgtdZvanQ5Fosmok",
+  authDomain: "fer201-cef24.firebaseapp.com",
+  projectId: "fer201-cef24",
+  storageBucket: "fer201-cef24.appspot.com",
+  messagingSenderId: "306946104122",
+  appId: "1:306946104122:web:547d6771d6393574a84e03",
+  measurementId: "G-MYRD2Y387Z",
+};
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
